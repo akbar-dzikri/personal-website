3858243bb240334dee1a286759c1e42600f0e21f
Akbar Dzikri <akbardzkr05@gmail.com>
2024-04-05 17:34:07 +0700

    mobile nav done

--- src/components/svelte/navbar/Navbar.svelte
+++ src/components/svelte/navbar/Navbar.svelte
@@ -8,9 +8,9 @@
   </div>
   <ul class="hidden sm:flex sm:justify-evenly sm:gap-2">
     <li><a href="/">Home</a></li>
-    <li><a href="#">Articles</a></li>
-    <li><a href="#">Projects</a></li>
-    <li><a href="#">About</a></li>
+    <li><a href="/articles">Articles</a></li>
+    <li><a href="/projects">Projects</a></li>
+    <li><a href="/about">About</a></li>
   </ul>
   <div class="relative sm:hidden">
     <NavMobile />
