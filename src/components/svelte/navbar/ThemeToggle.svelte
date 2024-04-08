c24a34e96fd780d5c5036c7817a352e418d3a18b
Akbar Dzikri <akbardzkr05@gmail.com>
2024-04-08 22:17:25 +0700

    fixed the js logic in themeToggle and added transition

--- src/components/svelte/navbar/ThemeToggle.svelte
+++ src/components/svelte/navbar/ThemeToggle.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { fade } from "svelte/transition";
+  import { cubicInOut } from "svelte/easing";
   import { onMount } from "svelte";
 
   let isDark: boolean;
@@ -10,15 +12,18 @@
       window.matchMedia &&
       window.matchMedia("prefer-color-scheme: dark").matches
     ) {
+      isDark = true;
       localStorage.setItem("theme", "dark");
-    } else if (!localTheme) {
-      localStorage.setItem("theme", "light");
+    } else {
+      if (!localTheme) {
+        isDark = false;
+        localStorage.setItem("theme", "light");
+      }
     }
     root?.setAttribute("data-theme", localTheme!);
   };
 
   const setTheme = () => {
-    isDark = !isDark;
     if (isDark) {
       root?.setAttribute("data-theme", "dark");
       localStorage.setItem("theme", "dark");
@@ -26,6 +31,7 @@
       root?.setAttribute("data-theme", "light");
       localStorage.setItem("theme", "light");
     }
+    isDark = !isDark;
   };
 
   onMount(() => {
@@ -33,18 +39,26 @@
   });
 </script>
 
-<button type="button" on:click={setTheme}>
+<button type="button" on:click={setTheme} class="flex items-center">
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    class="fill-colors-text w-6"
+    class="fill-colors-text w-5 sm:w-6"
   >
-    {#if !isDark}
+    {#if isDark}
       <path
+        transition:fade={{
+          duration: 300,
+          easing: cubicInOut,
+        }}
         d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"
       ></path>
     {:else}
       <path
+        transition:fade={{
+          duration: 300,
+          easing: cubicInOut,
+        }}
         d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2H2zm17 0h3v2h-3zM5.637 19.778l-1.414-1.414 2.121-2.121 1.414 1.414zM16.242 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.344 7.759 4.223 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"
       ></path>
     {/if}
