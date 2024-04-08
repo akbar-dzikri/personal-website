b266b49418b1daa64b9b811189f9f5892e0cb37c
Akbar Dzikri <akbardzkr05@gmail.com>
2024-04-08 23:07:48 +0700

    added contact links

--- src/components/svelte/navbar/NavLinks.svelte
+++ src/components/svelte/navbar/NavLinks.svelte
@@ -1,4 +1,5 @@
 <li><a href="/">Home</a></li>
+<li><a href="/about">About</a></li>
 <li><a href="/articles">Articles</a></li>
 <li><a href="/projects">Projects</a></li>
-<li><a href="/about">About</a></li>
+<li><a href="/contact">Contact</a></li>
