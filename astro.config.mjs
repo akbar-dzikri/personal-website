3d8586f8d64acde14b5c8399e334eb1f51542476
Akbar Dzikri <akbardzkr05@gmail.com>
2024-04-09 23:26:43 +0700

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sveltiaCms from "astro-sveltia-cms";
+import vercel from "@astrojs/vercel/serverless";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), svelte(), sveltiaCms()],
-  output: 'server'
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    svelte(),
+    sveltiaCms(),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
